f3e92512717b6fe68d80a8b07039c2982ae3a43f
gamze <gamzekandara@gmail.com>
2024-02-16 07:40:12 -0500

    Adds App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const handlePetClick = () => {
+    let apiUrl = "http://localhost:3001/pets";
+
+    if (filters.type !== "all") {
+      apiUrl += `?type=${filters.type}`;
+    }
+
+    fetch(apiUrl)
+      .then((r) => r.json())
+      .then((allPets) => setPets(allPets));
+  };
+
+  function handleChange(e) {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      type: e.target.value,
+    }));
+  }
+
+  function onAdoptPet(id) {
+    console.log("onAdoptPet called with id:", id);
+    const updatedPets = pets.map((pet) => {
+      if (pet.id === id) {
+        return { ...pet, isAdopted: true }; // Update the isAdopted property
+      }
+      return pet;
+    });
+    setPets(updatedPets);
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +44,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={handleChange}
+              onFindPetsClick={handlePetClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser setPets={setPets} pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
